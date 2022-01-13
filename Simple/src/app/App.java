3d588981372442899724d742d61a2790197d980e
Drachbar <Mattias.B.Andersson@outlook.com>
2022-01-13 21:15:54 +0100

    changed hello message

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -4,6 +4,6 @@ import javax.sound.sampled.SourceDataLine;
 
 class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world!!!");
     }
 }
