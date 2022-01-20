b082fbb5f322a7b3936f814399d737b68ab0263b
Drachbar <Mattias.B.Andersson@outlook.com>
2022-01-20 18:44:27 +0100

    added multiset of animals

--- guava-example/src/main/java/com/drachbar/examples/guava_example/App.java
+++ guava-example/src/main/java/com/drachbar/examples/guava_example/App.java
@@ -1,5 +1,8 @@
 package com.drachbar.examples.guava_example;
 
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Multiset<String> animals = HashMultiset.create();
+    	
+    	animals.add("Cat");
+    	animals.add("cat");
+    	animals.add("Dog");
+    	
+    	
+    	animals.forEach(System.out::println);
     }
 }
