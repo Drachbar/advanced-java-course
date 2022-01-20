b7d61ac1626d57c6d4541d0f3221cc1fbd8e7b7c
Drachbar <Mattias.B.Andersson@outlook.com>
2022-01-20 18:31:06 +0100

    Created

---
+++ guava-example/src/main/java/com/drachbar/examples/guava_example/App.java
@@ -0,0 +1,13 @@
+package com.drachbar.examples.guava_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
