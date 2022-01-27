f40a0996a5d51514ba5f3e32373af086f3310f07
Drachbar <Mattias.B.Andersson@outlook.com>
2022-01-27 21:46:59 +0100

    Implementing database statements

--- mysql/src/main/java/com/drachbar/db/mysql/App.java
+++ mysql/src/main/java/com/drachbar/db/mysql/App.java
@@ -9,21 +9,45 @@ import java.sql.SQLException;
 public class App {
 	public static void main(String[] args) {
 		var db = Database.instance();
-		
+
 		try {
 			db.connect();
 		} catch (SQLException e) {
 			System.out.println("Cannot connect to database");
 			e.printStackTrace();
 		}
-		
+
 		System.out.println("Connected");
-		
+
 		UserDao userDao = new UserDaoImpl();
+
+		// userDao.save(new User("Mars"));
+		// userDao.save(new User("Mercury"));
+		// userDao.save(new User("Neptune"));
+
+		var users = userDao.getAll();
+
+		users.forEach(System.out::println);
+
+		var userOpt = userDao.findById(4);
+
+		if (userOpt.isPresent()) {
+			
+			User user = userOpt.get();
+			
+			System.out.println("Retrieved: " + user);
+			user.setName("Snoopy");
+			
+			userDao.update(user);
+		} else {
+			System.out.println("No users retrieved");
+		}
+
+		userDao.delete(new User(5, null));
+		users = userDao.getAll();
+		users.forEach(System.out::println);
+
 		
-		//userDao.save(new User("Mars"));
-		//userDao.save(new User("Mercury"));
-		userDao.save(new User("Neptune"));
 		
 		try {
 			db.close();
--- mysql/src/main/java/com/drachbar/db/mysql/UserDaoImpl.java
+++ mysql/src/main/java/com/drachbar/db/mysql/UserDaoImpl.java
@@ -1,22 +1,80 @@
 package com.drachbar.db.mysql;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class UserDaoImpl implements UserDao{
+public class UserDaoImpl implements UserDao {
 
 	@Override
 	public void save(User u) {
 		var conn = Database.instance().getConnection();
-		
+
 		try {
 			var stmt = conn.prepareStatement("insert into user (name) values (?)");
-			
+
 			stmt.setString(1, u.getName());
+
+			stmt.executeUpdate();
+
+			stmt.close();
+		} catch (SQLException e) {
+			throw new DaoException(e);
+		}
+
+	}
+
+	@Override
+	public Optional<User> findById(int id) {
+		var conn = Database.instance().getConnection();
+
+		try {
+			var stmt = conn.prepareStatement("select name from user where id=?");
+
+			stmt.setInt(1, id);
+			var rs = stmt.executeQuery();
+
+			if(rs.next()) {
+				var name = rs.getString("name");
+				
+				User user = new User(id, name);
+				return Optional.of(user);
+			}
 			
+			stmt.close();
+		} catch (SQLException e) {
+			throw new DaoException(e);
+		}
+		
+		return Optional.empty();
+	}
+
+	@Override
+	public void update(User u) {
+		var conn = Database.instance().getConnection();
+		
+		try {
+			var stmt = conn.prepareStatement("update user set name=? where id=?");
+			stmt.setString(1, u.getName());
+			stmt.setInt(2, u.getId());
 			stmt.executeUpdate();
 			
+		} catch (SQLException e) {
+			throw new DaoException(e);
+		}
+	}
+
+	@Override
+	public void delete(User u) {
+		var conn = Database.instance().getConnection();
+		
+		PreparedStatement stmt;
+		try {
+			stmt = conn.prepareStatement("delete from user where id=?");
+			stmt.setInt(1, u.getId());
+			stmt.executeUpdate();
 			stmt.close();
 		} catch (SQLException e) {
 			throw new DaoException(e);
@@ -25,27 +83,28 @@ public class UserDaoImpl implements UserDao{
 	}
 
 	@Override
-	public Optional<User> findById(int id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void update(User t) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void delete(User t) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public List<User> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<User> users = new ArrayList<>();
+		var conn = Database.instance().getConnection();
+
+		try {
+			var stmt = conn.createStatement();
+
+			var rs = stmt.executeQuery("select id, name from user");
+
+			while(rs.next()) {
+				var id = rs.getInt("id");
+				var name = rs.getString("name");
+				
+				users.add(new User(id, name));
+			}
+			
+			stmt.close();
+		} catch (SQLException e) {
+			throw new DaoException(e);
+		}
+		
+		return users;
 	}
 
 }
