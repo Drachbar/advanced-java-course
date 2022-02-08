d665da2d73e0185e0f7b6d8d9a38d0e0046db739
Drachbar <Mattias.B.Andersson@outlook.com>
2022-02-08 21:52:09 +0100

    checking retrieved users

--- mysql/src/test/java/com/drachbar/db/mysql/UserDaoImplTest.java
+++ mysql/src/test/java/com/drachbar/db/mysql/UserDaoImplTest.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -45,7 +46,7 @@ public class UserDaoImplTest {
 	public void setUp() throws SQLException, IOException {
 
 		users = loadUsers();
-		
+
 		System.out.println(users);
 		System.out.println(users.size());
 
@@ -63,35 +64,72 @@ public class UserDaoImplTest {
 	public void tearDown() throws SQLException {
 		Database.instance().close();
 	}
-	
+
 	private int getMaxId() throws SQLException {
-		
+
 		var stmt = conn.createStatement();
-		
+
 		var rs = stmt.executeQuery("select max(id) as id from user");
-		
+
 		rs.next();
-		
+
 		var id = rs.getInt("id");
-		
+
 		stmt.close();
 		return id;
+	}
+
+	private List<User> getUsersInRange(int minId, int maxId) throws SQLException {
+
+		List<User> retrieved = new ArrayList<>();
+
+		var stmt = conn.prepareStatement("select id, name from user where id >= ? and id <= ?");
+
+		stmt.setInt(1, minId);
+		stmt.setInt(2, maxId);
+
+		var rs = stmt.executeQuery();
+
+		while (rs.next()) {
+			int id = rs.getInt("id");
+			String name = rs.getString("name");
+
+			var user = new User(id, name);
+
+			retrieved.add(user);
+		}
+
+		stmt.close();
+
+		return retrieved;
 	}
 
 	@Test
 	public void testSaveMultiple() throws SQLException {
 		UserDao userDao = new UserDaoImpl();
-		
-		for (var u: users) {
+
+		for (var u : users) {
 			userDao.save(u);
 		}
-		
+
 		var maxId = getMaxId();
-		
-		System.out.println(maxId);
-		
+
+		for (int i = 0; i < users.size(); i++) {
+			int id = (maxId - users.size()) + i + 1;
+
+			users.get(i).setId(id);
+		}
+
+		var retrievedUsers = getUsersInRange((maxId - users.size()) + 1, maxId);
+
+		assertEquals("Size of retrieved users not equal to number of test users", 
+				retrievedUsers.size(),
+				NUM_TEST_USERS);
+
+		assertEquals("retrieved users don't match saved users", users, retrievedUsers);
+
 	}
-	
+
 	@Test
 	public void testSave() throws SQLException {
 		User user = new User("Jupiter");
