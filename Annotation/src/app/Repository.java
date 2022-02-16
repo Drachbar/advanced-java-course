53a0ef9b280e762313f956c3ce1d2579607f42ee
Drachbar <Mattias.B.Andersson@outlook.com>
2022-02-16 21:21:04 +0100

    collect by @Field annotation

--- Annotation/src/app/Repository.java
+++ Annotation/src/app/Repository.java
@@ -1,7 +1,31 @@
 package app;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Repository<T> {
 	public void save(T t) {
-		System.out.println(t);
+		var clazz = t.getClass();
+		
+		var fields = clazz.getDeclaredFields();
+		
+		for(var field: fields) {
+			var annotation = field.getAnnotationsByType(Field.class);
+			
+			if(annotation.length > 0) {
+				System.out.println(field);
+			}
+		}
+		
+		//  @formatter:off
+			var fieldList = Arrays
+			.stream(clazz.getDeclaredFields())
+			.filter(f -> f.getAnnotationsByType(Field.class).length > 0)
+			.collect(Collectors.toList());
+		
+		// @formatter:on
+		
+			System.out.println();
+			System.out.println(fieldList);
 	}
 }
