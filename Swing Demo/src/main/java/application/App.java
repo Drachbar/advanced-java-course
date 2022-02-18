e41012604b66efd63a08499f08abba8d02a9466e
Drachbar <Mattias.B.Andersson@outlook.com>
2022-02-18 22:12:06 +0100

    Adding FlatLafDark Look-and-Feel

--- Swing Demo/src/main/java/application/App.java
+++ Swing Demo/src/main/java/application/App.java
@@ -4,14 +4,18 @@ import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+
 import controller.Controller;
 
 public class App {
 
 	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
 		
-		String NimbusLookAndFeel = ("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-		UIManager.setLookAndFeel(NimbusLookAndFeel);
+		//String NimbusLookAndFeel = ("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		
+		//UIManager.setLookAndFeel(NimbusLookAndFeel);
+		UIManager.setLookAndFeel(new FlatDarkLaf());
 		
 		SwingUtilities.invokeLater(Controller::new);
 		
