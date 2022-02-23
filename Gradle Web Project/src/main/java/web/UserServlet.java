b59f93b6de10450539f723ffeb667f16ad9d098a
Drachbar <Mattias.B.Andersson@outlook.com>
2022-02-23 20:16:47 +0100

    the Gson Library

--- Gradle Web Project/src/main/java/web/UserServlet.java
+++ Gradle Web Project/src/main/java/web/UserServlet.java
@@ -26,7 +26,7 @@ public class UserServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("get");
+		response.getWriter().println("hello");
 	}
 
 	/**
