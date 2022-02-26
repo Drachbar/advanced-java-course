c9e542a021696722b71afa250aeb1ab221d601b6
Drachbar <Mattias.B.Andersson@outlook.com>
2022-02-26 14:14:06 +0100

    Added component runner

--- Spring Boot Hello World/src/main/java/app/App.java
+++ Spring Boot Hello World/src/main/java/app/App.java
@@ -3,12 +3,11 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"main"})
 public class App {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
-
 	}
 
 }
