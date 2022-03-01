ef233de87997dc93b2a93584e129ea3241086507
Drachbar <Mattias.B.Andersson@outlook.com>
2022-03-01 19:43:40 +0100

    Beans

--- Spring Boot Hello World/src/main/java/main/Runner.java
+++ Spring Boot Hello World/src/main/java/main/Runner.java
@@ -1,15 +1,20 @@
 package main;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import greeters.Greeter;
+
 @Component
-public class Runner implements CommandLineRunner{
+public class Runner implements CommandLineRunner {
+
+	@Autowired
+	private Greeter greeter;
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello");
-		
+
+		greeter.greet();
 	}
-	
 }
