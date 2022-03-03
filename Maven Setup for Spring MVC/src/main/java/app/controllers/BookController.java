65118dfb72a322eb52258ce5522524d9d577e8f8
Drachbar <Mattias.B.Andersson@outlook.com>
2022-03-03 22:41:44 +0100

    Posting JSON to Spring

--- Maven Setup for Spring MVC/src/main/java/app/controllers/BookController.java
+++ Maven Setup for Spring MVC/src/main/java/app/controllers/BookController.java
@@ -1,6 +1,8 @@
 package app.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.entities.Book;
@@ -12,6 +14,14 @@ public class BookController {
 	public Book books() {
 		
 		var book = new Book("Charles Dickens", "Great expectations");
+		return book;
+	}
+	
+	@PostMapping("/books")
+	public Book createBook(@RequestBody Book book) {
+		
+		System.out.println("Creating: " + book);
+		book.setId(7L);
 		return book;
 	}
 }
