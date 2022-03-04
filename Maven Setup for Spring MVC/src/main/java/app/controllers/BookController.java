3c48a7aed56362ddd16371a6da73614363645d1c
Drachbar <Mattias.B.Andersson@outlook.com>
2022-03-05 00:45:51 +0100

    Implementing the book controller

--- Maven Setup for Spring MVC/src/main/java/app/controllers/BookController.java
+++ Maven Setup for Spring MVC/src/main/java/app/controllers/BookController.java
@@ -1,39 +1,40 @@
 package app.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.entities.Book;
+import app.services.BookService;
 
 @RestController
 public class BookController {
 	
+	@Autowired
+	private BookService bookService;
+	
 	@GetMapping("/books")
-	public Book books() {
+	public List<Book> books() {
 		
-		var book = new Book("Charles Dickens", "Great expectations");
-		return book;
+		return bookService.getAll();
 	}
 	
 	@PostMapping("/books")
-	public Book createBook(@RequestBody Book book) {
-		
-		System.out.println("Creating: " + book);
-		book.setId(7L);
-		return book;
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public void createBook(@RequestBody Book book) {
+		bookService.save(book);
 	}
 	
 	@GetMapping("/books/{id}")
 	public Book getBook(@PathVariable("id") Long id) {
-		var book = new Book("Charles Dickens", "Great expectations");
 		
-		System.out.println("Book ID: " + id);
-		
-		book.setId(id);
-		
-		return book;
+		return bookService.getBook(id);
 	}
 }
