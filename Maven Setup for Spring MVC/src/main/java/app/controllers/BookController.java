55a54ff61c3661725312d40f289d7034c6d484e5
Drachbar <Mattias.B.Andersson@outlook.com>
2022-03-04 22:45:58 +0100

    Path variables

--- Maven Setup for Spring MVC/src/main/java/app/controllers/BookController.java
+++ Maven Setup for Spring MVC/src/main/java/app/controllers/BookController.java
@@ -1,6 +1,7 @@
 package app.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,17 @@ public class BookController {
 		
 		System.out.println("Creating: " + book);
 		book.setId(7L);
+		return book;
+	}
+	
+	@GetMapping("/books/{id}")
+	public Book getBook(@PathVariable("id") Long id) {
+		var book = new Book("Charles Dickens", "Great expectations");
+		
+		System.out.println("Book ID: " + id);
+		
+		book.setId(id);
+		
 		return book;
 	}
 }
