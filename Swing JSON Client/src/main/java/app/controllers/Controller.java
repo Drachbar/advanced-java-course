0e2ea08dcf4cd9be2e6cbdd8a724ef5aa9d48326
Drachbar <Mattias.B.Andersson@outlook.com>
2022-03-07 20:57:50 +0100

    connecting to the service

--- Swing JSON Client/src/main/java/app/controllers/Controller.java
+++ Swing JSON Client/src/main/java/app/controllers/Controller.java
@@ -1,12 +1,38 @@
 package app.controllers;
 
-import gui.MainFrame;
+import java.io.IOException;
+
+import app.gui.CreateBookPanel;
+import app.gui.MainFrame;
+import app.gui.ViewBooksPanel;
+import services.BookService;
 
 public class Controller {
 	
 	private MainFrame mainFrame;
+	private CreateBookPanel createPanel;
+	private ViewBooksPanel viewPanel;
+	private BookService bookService;
 	
 	public Controller() {
-		mainFrame = new MainFrame();
+		
+		bookService = new BookService();
+		
+		try {
+			bookService.getAll();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		createPanel = new CreateBookPanel();
+		
+		createPanel.setFormListener((author, title) -> {
+			System.out.println(author + ": " + title);
+		});
+		
+		viewPanel = new ViewBooksPanel();
+		
+		mainFrame = new MainFrame(createPanel, viewPanel);
 	}
 }
